8e8eca2209daa48d9e2df21913cd583452b051e4
Emre Çalışkan <emre.caliskan@beyn.com.tr>
2023-12-28 17:13:40 +0300

    added health check endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/health-check', fn(Request $request) => Response::noContent());
